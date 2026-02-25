17b55cf354e603aa26ba49cb05a38b8f97616ca9
Castello <guiaugusto.castello@gmail.com>
2026-02-25 20:43:16 -0300

    Deploy: V3.0.0.0

--- config/gcs.js
+++ config/gcs.js
@@ -1,4 +1,8 @@
 import { Storage } from "@google-cloud/storage";
+import { GCP_KEY_PATH } from "./gcs";
 
-export const storage = new Storage();
+export const storage = new Storage({
+  keyFilename: GCP_KEY_PATH,
+  projectId: "weatherproject-486222",
+});
 export const bucket = storage.bucket("all2bsafe-images");
---
+++ scripts/generateGCPSA.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import fs from "fs";
+import path from "path";
+
+const gcpKeyPath = path.join("/tmp", "gcp-sa.json");
+
+if (!fs.existsSync(gcpKeyPath)) {
+  console.log(process.env)
+  if (!process.env.GCP_SA_JSON) {
+    throw new Error("GCP_SA_JSON não está definida");
+  }
+
+  fs.writeFileSync(
+    gcpKeyPath,
+    process.env.GCP_SA_JSON,
+    { encoding: "utf-8" }
+  );
+}
+
+console.log("Service Account gerado em:", gcpKeyPath);
