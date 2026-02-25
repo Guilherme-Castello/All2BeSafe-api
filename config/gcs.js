c8154eb758763c281adfc947c214b1eb8ef7bb24
Castello <guiaugusto.castello@gmail.com>
2026-02-25 20:58:56 -0300

    Deploy: V3.0.0.2

--- config/gcs.js
+++ config/gcs.js
@@ -1,8 +1,4 @@
 import { Storage } from "@google-cloud/storage";
-import { GCP_KEY_PATH } from "./gcs";
 
-export const storage = new Storage({
-  keyFilename: GCP_KEY_PATH,
-  projectId: "weatherproject-486222",
-});
+export const storage = new Storage();
 export const bucket = storage.bucket("all2bsafe-images");
